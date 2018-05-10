e853313f9566854bf81aca92b4fdd2cc26841c9d
CunjunWang <13621691063@163.com>
2018-05-09 17:22:48 -0700

    path setup

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-    res.render("index.html");
+    res.render("index")
 });
 
 const port = process.env.PORT || 8080;
